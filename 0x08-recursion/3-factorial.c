d3d09d639ead8365223d0c2c096c2a2766303048
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-03-08 17:05:27 -0800

    fixed the betty coding style error

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * factorial - returns the factorial of a given number.
  * @n: input number.
@@ -8,15 +7,10 @@
  */
 int factorial(int n)
 {
-       if (n<0)
-       {
-	       return (-1);
-       }
-       else if (n==0)
-       {
-	       return (1);
-       }
-       else
-	       return (n * factorial(n-1));
-
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
 }
