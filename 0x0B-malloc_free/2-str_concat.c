265ca47840c4469de1cd67a234ef91a74b048b51
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-03-13 18:05:42 -0700

    betty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,54 +2,57 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: Pointer to first string.
- * @s2: Pointer to second string.
- *
- * Return: Pointer to concatenated string or NULL on failure.
+ * str_concat - A function that concatenates two strings
+ * @s1: An input pointer of the first string
+ * @s2: An input pointer of the second string
+ * Return: A pointer to the concatenated strings or NULL if it's NULL
  */
+
 char *str_concat(char *s1, char *s2)
 {
-        char *new_str, *starts1, *starts2;
-        int i = 0, lens1 = 0, lens2 = 0;
+	char *new_str, *starts1, *starts2;
+	int i = 0, lens1 = 0, lens2 = 0;
 
-        starts1 = s1;
-        starts2 = s2;
+	starts1 = s1;
+	starts2 = s2;
 
-        if (s1 == NULL)
-                s1 = "";
-        while (*s1)
-        {
-                lens1++;
-                s1++;
-        }
-        s1 = starts1;
-        if (s2 == NULL)
-                s2 = "";
-        while (*s2)
-        {
-                lens2++;
-                s2++;
-        }
-        s2 = starts2;
-        new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
-        starts1 = new_str;
-        if (new_str == NULL)
-                return (NULL);
-        for (; i < (lens1 + lens2); i++)
-        {
-                if (i < lens1)
-                {
-                        new_str[i] = *s1;
-                        s1++;
-                }
-                else
-                {
-                        new_str[i] = *s2;
-                        s2++;
-                }
-        }
-        new_str[i] = '\0';
-        return (starts1);
+	if (s1 == NULL)
+		s1 = "";
+	while (*s1)
+	{
+		lens1++;
+		s1++;
+	}
+	s1 = starts1;
+
+	if (s2 == NULL)
+		s2 = "";
+	while (*s2)
+	{
+		lens2++;
+		s2++;
+	}
+	s2 = starts2;
+
+	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	starts1 = new_str;
+	if (new_str == NULL)
+		return (NULL);
+
+	for (; i < (lens1 + lens2); i++)
+	{
+		if (i < lens1)
+		{
+			new_str[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[i] = *s2;
+			s2++;
+		}
+	}
+	new_str[i] = '\0';
+	return (starts1);
 }
 
