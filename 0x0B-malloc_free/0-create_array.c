8abbf93c37d9f0adc205e86f72109132fbea841f
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-03-13 17:29:25 -0700

    first task correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,23 +10,21 @@
  * Return: If size == 0 or the function fails - NULL.
  *         Otherwise - a pointer to the array.
  */
-
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int index;
 
 	if (size == 0)
-		return NULL;
+		return (NULL);
+
 	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
-		return NULL;
+		return (NULL);
 
-	for (index = 0; index < size ; index++)
-		array(index) = c;
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
-	return index;
-
+	return (array);
 }
-
