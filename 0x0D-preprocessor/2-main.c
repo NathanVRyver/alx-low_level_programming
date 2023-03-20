6591e7a746ad252a137cd1b1a186d2112379672d
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-03-20 16:34:49 -0700

    defined the function that returns the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * A function that returns the name of the file it was compiled from 
+ *
+ * Return: __FILE__
+ *
+ * PART OF THE ALX PREPROCESSOR PROJECT
+ */
+
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
