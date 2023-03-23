d91079fb3c98a7ff8bf00bdf2896ed7ff8633201
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-03-23 13:08:32 -0700

    fixed some betty coding style issues

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,26 +2,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
-
 /**
- * print_numbers: function that prints numbers
- * @seperator: number seperator
- * @n: number count
- * return: void
- */
+*print_numbers - a function that print numbers
+*@separator: number seperator
+*@n: number counts
+*Return: void
+*/
 
 
-void print_numbers(const char *seperator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list ap;
 
 	va_start(ap, n);
-	for (i=0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ap, int));
-		if ((seperator) && (i < n-1))
-			printf("%s", seperator);
+		if ((separator) && (i < n - 1))
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(ap);
