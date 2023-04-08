414b902c503c11391451e8c1fbbd93873a8fbf90
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-04-08 08:41:16 -0700

    implementation didn't pass all checks, so I tried adjusting the code and tested it again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,31 @@
 #include "main.h"
 #include <string.h>
 /*
+ * binary_to_uint -- converts a binary number to an unsigned int
  * return: 0 if the value is null or is not binary
  * @converted_value: the value that was converted from binary
- * @alert: a variable to pick up if the array has a value that isn't 1 or 0
+ * @b: pointer to the binary string
  * @i: counter variable for the for loop
  * @len: the length of the array of chars to be converted
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	int alert = 0;
 	unsigned int converted_value = 0;
-	int len = strlen(b);
-	int i;
+	unsigned int bit_value = 1;
+	unsigned int len = strlen(b);
+	unsigned int i;
 
 	if (b == NULL)
 		return (0);
+
 	for (i = 0; i < len; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
-			alert++;
-		else if (b[i] == '1')
-			converted_value += 1 << (len - i - 1);
+			return (0);
+		if (b[i] == '1')
+			converted_value += bit_value;
+		bit_value <<= 1;
 	}
-	if (alert > 1)
-		return (0);
 	return (converted_value);
 }
