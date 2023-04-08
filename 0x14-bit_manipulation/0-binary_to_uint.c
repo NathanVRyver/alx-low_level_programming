c601c4e498b29de068ff7cf92de9bcc6a9fad83d
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-04-08 08:22:23 -0700

    tested the code, and then fixed some Betty coding style small errors - task finished

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,6 +4,8 @@
  * return: 0 if the value is null or is not binary
  * @converted_value: the value that was converted from binary
  * @alert: a variable to pick up if the array has a value that isn't 1 or 0
+ * @i: counter variable for the for loop
+ * @len: the length of the array of chars to be converted
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -11,16 +13,18 @@ unsigned int binary_to_uint(const char *b)
 	int alert = 0;
 	unsigned int converted_value = 0;
 	int len = strlen(b);
+	int i;
+
 	if (b == NULL)
 		return (0);
-	for (int i = 0; i < len; i++)
+	for (i = 0; i < len; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			alert++;
-		else if (b[i] == '1') 
+		else if (b[i] == '1')
 			converted_value += 1 << (len - i - 1);
 	}
 	if (alert > 1)
-		return 0;
-	return converted_value;
+		return (0);
+	return (converted_value);
 }
