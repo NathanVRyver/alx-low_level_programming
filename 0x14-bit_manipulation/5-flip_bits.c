f2ee5bb17d55874e2181d60eca9cc1ecf772f1d6
“01010110-Code” <tantaouibahaabusiness@gmail.com>
2023-04-11 14:11:56 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns the number of bits needed to flip to get from one number to another.
+ * @n: First number.
+ * @m: Second number.
+ *
+ * Return: Number of bits needed to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flip = 0, count = 0;
+
+	flip = n ^ m;
+	while (flip)
+	{
+		if (flip & 1)
+			count++;
+
+		flip >>= 1;
+	}
+
+	return (count);
+}
